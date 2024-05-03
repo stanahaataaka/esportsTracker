ac5135f1f2977afdc12585d39263df8073169efc
Mitchell Lageson <mitchlageson@gmail.com>
2024-05-02 21:50:28 -0500

    added test add, create, and delete functions

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('teams/', views.TeamListView.as_view(), name='teams'),
     path('teams/<str:pk>', views.TeamDetailView.as_view(), name='teams'),
-<<<<<<< HEAD
-    path("admin_tracker/", views.admin_tracker, name="admin_tracker")
-=======
+    path("admin_tracker/", views.admin_tracker, name="admin_tracker"),
     path('players/', views.PlayerListView.as_view(), name='players'),
->>>>>>> main
+]
+
+urlpatterns += [
+    path('teams/create/', views.TeamCreate.as_view(), name='author-create'),
+    path('teams/<str:pk>/update/', views.TeamUpdate.as_view(), name='author-update'),
+    path('teams/<str:pk>/delete/', views.TeamDelete.as_view(), name='author-delete'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Team, Game, Player
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.urls import reverse
 
 def index(request):
     #IOU needed homepage info
@@ -13,11 +19,34 @@ class TeamListView(generic.ListView):
 class TeamDetailView(generic.DetailView):
     model = Team
 
-<<<<<<< HEAD
 def admin_tracker(request):
     return render(request, "admin_tracker.html")
-=======
+
 class PlayerListView(generic.ListView):
     model = Player
     paginate_by = 10
->>>>>>> main
+
+class TeamCreate(PermissionRequiredMixin, CreateView):
+    model = Team
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'tracker.add_team'
+
+class TeamUpdate(PermissionRequiredMixin, UpdateView):
+    model = Team
+    fields = '__all__'
+    permission_required = 'tracker.change_team'
+
+class TeamDelete(PermissionRequiredMixin, DeleteView):
+    model = Team
+    success_url = reverse_lazy('teams')
+    permission_required = 'tracker.delete_team'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("team-delete", kwargs={"pk": self.object.pk})
+            )
